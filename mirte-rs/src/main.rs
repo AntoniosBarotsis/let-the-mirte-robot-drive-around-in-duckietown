46d6e15e40a2754d059c5338266052ae793c0642
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-05-12 21:47:13 +0200

    Do fancy stuff

--- mirte-rs/src/main.rs
+++ mirte-rs/src/main.rs
@@ -1,3 +1,11 @@
+use cv::show_in_window;
+use ros::{process_ros_image, CvImage};
+
+/// For now, just reads an image from ROS and shows it on screen.
+#[allow(clippy::unwrap_used)]
 fn main() {
-  println!("Hello, world!");
+  process_ros_image(|img| {
+    let mat = CvImage::from_imgmsg(img).unwrap().as_cvmat().unwrap();
+    show_in_window(&mat);
+  });
 }
