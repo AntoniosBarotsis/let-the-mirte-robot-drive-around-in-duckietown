44bdb4a813624e1f0176fbccc6e7626b0b89e77f
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-05-15 16:00:38 +0200

    Add save to disk example

---
+++ cv/examples/save_to_disk.rs
@@ -0,0 +1,21 @@
+use cv::{cv_error::CvError, process_image};
+
+use opencv::{
+  core::Vector,
+  imgcodecs::{imread, imwrite, IMREAD_GRAYSCALE},
+};
+
+/// This example performs line detection and saves the image to disk as "`assets/input_real.jpg`".
+fn main() {
+  // We need to import the image as grayscale because the FastLineDetector requires it.
+  let img = imread("./assets/input_real.jpg", IMREAD_GRAYSCALE).expect("open image");
+
+  let output = process_image(img).expect("Process image");
+
+  // Save output image.
+  let saved = imwrite("./assets/output.jpg", &output, &Vector::default())
+    .map_err(|e| CvError::IoError(e.message)).expect("Save works");
+
+  // Make sure that the image was saved correctly
+  assert!(saved);
+}
