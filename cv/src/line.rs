00db0c850117359edddd1236b04091247e337170
Kees Blok <C.Blok-1@student.tudelft.nl>
2023-05-16 14:20:31 +0200

    Remove warnings

--- cv/src/line.rs
+++ cv/src/line.rs
@@ -1,14 +1,20 @@
+// Represents a line color
+#[derive(Debug, Clone, Copy)]
 pub enum Color {
   Yellow,
   White,
   Red
 }
 
+// Represents a end coordinate of a line
+#[derive(Debug, Clone, Copy)]
 pub struct Pos {
   x: f64,
   y: f64,
 }
 
+// Represents a line
+#[derive(Debug, Clone, Copy)]
 pub struct Line {
   color: Color,
   pos1: Pos,
