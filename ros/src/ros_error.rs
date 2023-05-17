e7f5a13649cb12f5cdaa194968c4f76c81e30692
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-05-17 15:37:22 +0200

    Fixed motor value error message

--- ros/src/ros_error.rs
+++ ros/src/ros_error.rs
@@ -1,9 +1,10 @@
+use crate::drive::{MOTOR_VALUE_MAX, MOTOR_VALUE_MIN};
 use thiserror::Error;
 
 /// Represents any errors that can occur while performing ROS related tasks.
 #[derive(Error, Debug)]
 pub enum RosError {
-  #[error("Expected a value between 0 and 100, got: {actual}.")]
+  #[error("Expected a value between {MOTOR_VALUE_MIN} and {MOTOR_VALUE_MAX}, got: {actual}.")]
   InvalidMotorValue { actual: i32 },
   #[error("Something went wrong while instantiating the \"{name}\" ROS client: {message}")]
   ClientCreation { name: String, message: String },
