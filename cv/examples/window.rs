f83b06fb492343bf254f34fc6fae76a09c7a821b
Dennis Heijmans <D.Heijmans@student.tudelft.nl>
2023-05-17 16:18:19 +0200

    Shift lines down to original position

--- cv/examples/window.rs
+++ cv/examples/window.rs
@@ -1,6 +1,6 @@
+use cv::detect_line_type;
 use cv::draw_lines::{draw_lines, read_image};
 use cv::line::Colour::{White, Yellow};
-use cv::{crop_image, detect_line_type};
 use std::time::Instant;
 
 /// Processes the input image from the assets folder and displays it in a window for an easier
@@ -15,7 +15,5 @@ fn main() {
 
   println!("{:?}", now.elapsed());
 
-  let mut draw_img = crop_image(&mut img, cv::image_part::ImagePart::Bottom).expect("crop image");
-
-  draw_lines(&mut draw_img, lines);
+  draw_lines(&mut img, lines);
 }
