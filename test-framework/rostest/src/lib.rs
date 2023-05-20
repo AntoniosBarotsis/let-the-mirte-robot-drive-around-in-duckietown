dc2a237a62f79f270d21b6ea4a8aadf82727d48e
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-05-21 01:39:13 +0200

    Make ros_test macro

--- test-framework/rostest/src/lib.rs
+++ test-framework/rostest/src/lib.rs
@@ -1,6 +1,20 @@
-use proc_macro::*;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
 
+/// Currently marks the method as a test that will be ran serially.
 #[proc_macro_attribute]
-pub fn rostest(attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+pub fn ros_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
+  // Parse the input function
+  let input = parse_macro_input!(item as ItemFn);
+
+  // Generate some new code to replace the original function
+  let new_code = quote! {
+      #[test]
+      #[serial_test::serial]
+      #input
+  };
+
+  // Return the new code as a TokenStream
+  TokenStream::from(new_code)
 }
