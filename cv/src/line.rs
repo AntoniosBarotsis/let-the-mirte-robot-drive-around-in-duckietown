0aa30f6851838f1efba5b13fe9acd82a57550779
Elias Hoste <e.r.hoste@student.tudelft.nl>
2023-05-24 18:31:17 +0200

    add vector tests

--- cv/src/line.rs
+++ cv/src/line.rs
@@ -1,4 +1,5 @@
 use derive_more::{Add, Div, Mul, Neg, Sub, Sum};
+use float_cmp::approx_eq;
 
 // H: 0-179, S: 0-255, V: 0-255
 pub static HSV_YELLOW: &[[u8; 3]; 2] = &[[25, 50, 50], [45, 255, 255]];
@@ -124,26 +125,39 @@ impl Vector {
   }
 
   pub fn slope(&self) -> f32 {
-    // If self.x is 0, the slope is invalid, and it means the line must be stictly vertical, which means simply
-    // returning a high value suffices for the calculations we are doing.
-    let slope = self.dir.y / self.dir.x;
-    if slope.is_nan() {
-      return 1000.0;
+    self.dir.y / self.dir.x
+  }
+
+  pub fn clamped_slope(&self) -> f32 {
+    let slope = self.slope();
+    if slope.is_nan() || slope.is_infinite() || slope > 100_000_000_000.0 {
+      100_000_000_000.0
+    } else {
+      slope
     }
-    slope
+  }
+
+  /// Returns the y value at the given x value on the vector
+  fn y(&self, x: f32) -> f32 {
+    self.slope() * (x - self.origin.x) + self.origin.y
   }
 
   pub fn intersect(&self, other: &Vector) -> Option<Pos> {
-    let slope1 = self.slope();
-    let slope2 = other.slope();
+    // We clamp the slopes to still get a valid output even if the slopes are infinite or NaN
+    let slope1 = self.clamped_slope();
+    let slope2 = other.clamped_slope();
+    // If slopes are the same, x will be invalid because the lines never intersect.
+    if approx_eq!(f32, slope1, slope2, ulps = 2) {
+      return None;
+    }
 
     let x = (self.origin.x * slope1 - self.origin.y - other.origin.x * slope2 + other.origin.y)
       / (slope1 - slope2);
-    // If (slope1 - slope2) is 0, x will be invalid because the lines never intersect.
-    if x.is_nan() {
-      return None;
-    }
-    let y = slope1 * (x - self.origin.x) + self.origin.y;
+    let y = if self.slope().is_infinite() {
+      other.y(x)
+    } else {
+      self.y(x)
+    };
 
     Some(Pos { x, y })
   }
@@ -151,13 +165,94 @@ impl Vector {
 
 #[cfg(test)]
 mod tests {
-  use crate::Pos;
-  use float_cmp::approx_eq;
+  use crate::line::{Dir, Pos, Vector};
+  use float_cmp::assert_approx_eq;
+
+  fn assert_float_eq(f1: f32, f2: f32) {
+    assert_approx_eq!(f32, f1, f2, ulps = 0);
+  }
+
+  fn assert_pos_eq(pos1: Pos, pos2: Pos) {
+    assert_float_eq(pos1.x, pos2.x);
+    assert_float_eq(pos1.y, pos2.y);
+  }
+
+  fn assert_dir_eq(dir1: Dir, dir2: Dir) {
+    assert_float_eq(dir1.x, dir2.x);
+    assert_float_eq(dir1.y, dir2.y);
+  }
 
   #[test]
   fn create_pos() {
     let pos = Pos::new(10.0, -5.0);
-    assert!(approx_eq!(f32, pos.x, 10.0, ulps = 2));
-    assert!(approx_eq!(f32, pos.y, -5.0, ulps = 2));
+    assert_float_eq(pos.x, 10.0);
+    assert_float_eq(pos.y, -5.0);
+  }
+
+  #[test]
+  fn create_vector() {
+    let vec = Vector::new(Pos::new(6.5, 3.0), Dir::new(1.0, 1.0));
+    assert_pos_eq(vec.origin, Pos::new(6.5, 3.0));
+    assert_dir_eq(vec.dir, Dir::new(1.0, 1.0));
+  }
+
+  #[test]
+  fn vector_midpoint() {
+    let vec = Vector::new(Pos::new(5.0, 0.0), Dir::new(0.0, 10.0));
+    let midpoint = vec.midpoint();
+    assert_pos_eq(midpoint, Pos::new(5.0, 5.0));
+  }
+
+  #[test]
+  fn vector_end() {
+    let vec = Vector::new(Pos::new(5.0, 0.0), Dir::new(0.0, 10.0));
+    let end = vec.end();
+    assert_pos_eq(end, Pos::new(5.0, 10.0));
+  }
+
+  #[test]
+  fn vector_inf_slope() {
+    let vec = Vector::new(Pos::new(5.0, 0.0), Dir::new(0.0, 10.0));
+    let slope = vec.slope();
+    assert_float_eq(slope, f32::INFINITY);
+  }
+
+  #[test]
+  fn vector_slope() {
+    let vec = Vector::new(Pos::new(5.0, 0.0), Dir::new(1.0, 10.0));
+    let slope = vec.slope();
+    assert_float_eq(slope, 10.0);
+  }
+
+  #[test]
+  fn intersect() {
+    let vec1 = Vector::new(Pos::new(4.0, 0.0), Dir::new(1.0, 1.0));
+    let vec2 = Vector::new(Pos::new(0.0, 0.0), Dir::new(1.0, -1.0));
+    let intersection = vec1.intersect(&vec2).expect("No intersection found!");
+    assert_pos_eq(intersection, Pos::new(2.0, -2.0));
+  }
+
+  #[test]
+  fn intersect_with_vertical() {
+    let vec1 = Vector::new(Pos::new(4.0, 0.0), Dir::new(0.0, 1.0));
+    let vec2 = Vector::new(Pos::new(0.0, 0.0), Dir::new(1.0, 1.0));
+    let intersection = vec1.intersect(&vec2).expect("No intersection found!");
+    assert_pos_eq(intersection, Pos::new(4.0, 4.0));
+  }
+
+  #[test]
+  fn intersect_with_identical() {
+    let vec1 = Vector::new(Pos::new(4.0, 0.0), Dir::new(1.0, 1.0));
+    let vec2 = Vector::new(Pos::new(4.0, 0.0), Dir::new(1.0, 1.0));
+    let intersection = vec1.intersect(&vec2);
+    assert!(intersection.is_none());
+  }
+
+  #[test]
+  fn intersect_with_same_slope() {
+    let vec1 = Vector::new(Pos::new(4.0, 0.0), Dir::new(1.0, 1.0));
+    let vec2 = Vector::new(Pos::new(8.0, 0.0), Dir::new(1.0, 1.0));
+    let intersection = vec1.intersect(&vec2);
+    assert!(intersection.is_none());
   }
 }
