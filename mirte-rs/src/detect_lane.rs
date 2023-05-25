c692c13dcc015d7186dea8252c746bb9af77b6be
Elias Hoste <e.r.hoste@student.tudelft.nl>
2023-05-25 15:22:59 +0200

    clean up code

--- mirte-rs/src/detect_lane.rs
+++ mirte-rs/src/detect_lane.rs
@@ -1,5 +1,6 @@
 use cv::line::{Colour, Dir, Line, Pos, Vector};
 
+/// Averages all lines with a given colour weighted by their length
 fn get_average_line(lines: &[Line], colour: Colour) -> Option<Vector> {
   let coloured_lines: Vec<Line> = lines
     .iter()
@@ -16,6 +17,7 @@ fn get_average_line(lines: &[Line], colour: Colour) -> Option<Vector> {
     .iter()
     .map(|line| line.direction().squared_length())
     .sum();
+
   let average_midpoint: Pos = coloured_lines
     .iter()
     .fold(Pos { x: 0.0, y: 0.0 }, |pos, line| {
@@ -29,6 +31,7 @@ fn get_average_line(lines: &[Line], colour: Colour) -> Option<Vector> {
   })
 }
 
+/// Estimates the lane given a line, which should either be a yellow or white line from Duckietown.
 fn estimate_lane(line: Vector) -> Line {
   // For now, simply returning the given vector may be enough. This will only become clear once we
   // have working motor controls for Mirte based on the lane. If it doesn't work well, we may need
@@ -50,69 +53,62 @@ pub fn lines_on_right(lines: &[Line], vector: &Vector) -> Vec<Line> {
     .collect()
 }
 
+/// Returns the bisection between `vec1` and `vec2`, if it exists
+pub fn bisect(vec1: &Vector, vec2: &Vector) -> Option<Vector> {
+  vec2.intersect(vec1).map(|intersection| {
+    let dir1 = vec1.dir;
+    let dir2 = vec2.dir;
+    let dir = dir1 * dir2.length() + dir2 * dir1.length();
+    Vector::new(intersection - Pos::from_dir(dir), dir * 2.0)
+  })
+}
+
+/// Detects the lane based on given line segments. Returns a `Vec<Line>`, where the first line is
+/// the lane, and any other lines are debug information for rendering to the screen. For example,
+/// if it detects a yellow and/or white line, it'll add those lines to be rendered.
 pub fn detect_lane(lines: &[Line]) -> Result<Vec<Line>, &'static str> {
   // Try to detect yellow line in image
   if let Some(y_vec) = get_average_line(lines, Colour::Yellow) {
-    eprintln!("Yellow line found! Looking for white lines to the right of the yellow line...");
-    // Get lines right of yellow line
+    // Get all lines that lie right of the yellow line
     let right_lines: Vec<Line> = lines_on_right(lines, &y_vec);
     // Try to detect white line right of yellow line
     if let Some(w_vec) = get_average_line(&right_lines, Colour::White) {
-      eprintln!("Right white line found! Trying to find intersection...");
       // Do a bisection with yellow and white line
-      // Calculate slope of bisection line
-      let lane = if let Some(intersection) = w_vec.intersect(&y_vec) {
-        eprintln!("Intersection found! Calculating lane...");
-        let y_dir = y_vec.dir;
-        let w_dir = w_vec.dir;
-        let dir = y_dir * w_dir.length() + w_dir * y_dir.length();
-        Line::from_vector(
-          Vector::new(intersection - Pos::from_dir(dir), dir * 2.0),
-          Colour::Green,
-        )
-      } else {
-        eprintln!(
-          "No intersection found! Using average of line positions instead. Calculating lane..."
-        );
+      let lane = bisect(&y_vec, &w_vec).unwrap_or({
         // If there is no intersection, both lines must have the same slope. Thus, we can simply
         // get the slope of one of the lines. Since there is no intersection, we can simply
         // estimate the centre of the lines by averaging their midpoints.
         let intersection = (y_vec.midpoint() + w_vec.midpoint()) / 2.0;
-        Line::from_vector(
-          Vector::new(intersection - Pos::from_dir(y_vec.dir), y_vec.dir * 2.0),
-          Colour::Green,
-        )
-      };
+        Vector::new(intersection - Pos::from_dir(y_vec.dir), y_vec.dir * 2.0)
+      });
 
-      eprintln!("Lane calculated: {:?}, {:?}", lane.start, lane.end);
       Ok(vec![
+        Line::from_vector(lane, Colour::Green),
         Line::from_vector(y_vec, Colour::Orange),
         Line::from_vector(w_vec, Colour::Black),
-        lane,
       ])
     } else {
-      eprintln!("No right white line found! Estimating lane based on yellow line...");
       // If no white line right of yellow line found, try to estimate lane based on yellow line
       Ok(vec![
-        Line::from_vector(y_vec, Colour::Orange),
         estimate_lane(y_vec),
+        Line::from_vector(y_vec, Colour::Orange),
       ])
     }
   } else {
-    eprintln!("No yellow line found! Looking for white lines in image...");
+    // If no yellow line is found, it's probably in a turn where the yellow lines are out of view.
+    // In that case, we can look for a white line on the right of the screen and estimate the lane
+    // using it.
     // Get lines on right side of screen (640x480)
     // TODO: get middle of screen from cv crate
     let middle_vec = Vector::new(Pos::new(350.0, 0.0), Dir::new(0.0, 1000.0));
     let right_lines: Vec<Line> = lines_on_right(lines, &middle_vec);
     // If no yellow line found, try to detect white line in image
     if let Some(w_vec) = get_average_line(&right_lines, Colour::White) {
-      eprintln!("White line found! Estimating lane based on white line...");
       return Ok(vec![
-        Line::from_vector(w_vec, Colour::Black),
         estimate_lane(w_vec),
+        Line::from_vector(w_vec, Colour::Black),
       ]);
     }
-    eprintln!("No yellow or white lines found! Unable to detect lane.");
     // If no yellow or white lines found, return error
     Err("Unable to detect lane without yellow or white lines!")
   }
