d641ba936c19e1263c4d8fdd4e3a436812ff2747
Dennis Heijmans <D.Heijmans@student.tudelft.nl>
2023-05-30 21:14:30 +0200

    Rename variables to be more clear

--- mirte-rs/src/detect_lane.rs
+++ mirte-rs/src/detect_lane.rs
@@ -2,7 +2,9 @@ use cv::line::{Colour, Line, LineSegment, Point, Vector};
 use Colour::{Black, Green, Orange, White, Yellow};
 
 // The minimum length of an average line for it to be significant
-const THRESHOLD: f32 = 0.15;
+const MINIMUM_LENGTH: f32 = 0.15;
+// The slope at which the direction of line should flip
+const DIRECTION_SLOPE: f32 = 0.25;
 
 /// Averages all lines with a given colour weighted by their length
 fn get_average_line(lines: &[LineSegment], colour: Colour) -> Option<Line> {
@@ -21,7 +23,7 @@ fn get_average_line(lines: &[LineSegment], colour: Colour) -> Option<Line> {
     .iter()
     .map(|line| -> Vector {
       let sign: f32 = if line.colour == White { -1.0 } else { 1.0 };
-      let threshold: Line = Line::from_dir(Vector::new(sign, 1.0));
+      let threshold: Line = Line::from_dir(Vector::new(sign, DIRECTION_SLOPE));
       if lies_on_right(&Point::from_vector(line.direction()), &threshold) {
         line.direction() * sign
       } else {
@@ -29,7 +31,7 @@ fn get_average_line(lines: &[LineSegment], colour: Colour) -> Option<Line> {
       }
     })
     .sum();
-  if weighted_dir.length() < THRESHOLD {
+  if weighted_dir.length() < MINIMUM_LENGTH {
     return None;
   }
 
@@ -53,11 +55,11 @@ pub fn lies_on_right(point: &Point, line: &Line) -> bool {
   point.x > line.origin.x + (point.y - line.origin.y) / line.slope()
 }
 
-/// Returns all lines in `lines` that lie to the right of `vector`
-pub fn lines_on_right(lines: &[LineSegment], vector: &Line) -> Vec<LineSegment> {
+/// Returns all lines in `lines` that lie to the right of `boundary`
+pub fn lines_on_right(lines: &[LineSegment], boundary: &Line) -> Vec<LineSegment> {
   lines
     .iter()
-    .filter(|line| lies_on_right(&line.midpoint(), vector))
+    .filter(|line| lies_on_right(&line.midpoint(), boundary))
     .copied()
     .collect()
 }
@@ -82,29 +84,29 @@ pub fn detect_lane(lines: &[LineSegment]) -> Option<LineSegment> {
 /// For example, if it detects a yellow and/or white line, it'll add those lines to be rendered.
 pub fn detect_lane_debug(lines: &[LineSegment]) -> Option<Vec<LineSegment>> {
   // Try to detect yellow line in image
-  let y_vec = get_average_line(lines, Yellow)
+  let yellow_line = get_average_line(lines, Yellow)
     .unwrap_or(Line::new(Point::new(0.0, 1.0), Vector::new(0.25, -1.0)));
   // Get all lines that lie right of the yellow line
-  let right_lines: Vec<LineSegment> = lines_on_right(lines, &y_vec);
+  let right_lines: Vec<LineSegment> = lines_on_right(lines, &yellow_line);
   // Try to detect white line right of yellow line
-  let w_vec = get_average_line(&right_lines, White)
+  let white_line = get_average_line(&right_lines, White)
     .unwrap_or(Line::new(Point::new(1.0, 1.0), Vector::new(-0.25, -1.0)));
   // Do a bisection with yellow and white line
-  let lane = bisect(&y_vec, &w_vec).unwrap_or({
+  let lane = bisect(&yellow_line, &white_line).unwrap_or({
     // If there is no intersection, both lines must have the same slope. Thus, we can simply
     // get the slope of one of the lines. Since there is no intersection, we can simply
     // estimate the centre of the lines by averaging their midpoints.
-    let intersection = (y_vec.origin + w_vec.origin) / 2.0;
+    let intersection = (yellow_line.origin + white_line.origin) / 2.0;
     Line::new(
-      intersection - Point::from_vector(y_vec.dir),
-      y_vec.dir * 2.0,
+      intersection - Point::from_vector(yellow_line.dir),
+      yellow_line.dir * 2.0,
     )
   });
 
   Some(vec![
     LineSegment::from_line(lane, Green),
-    LineSegment::from_line(y_vec, Orange),
-    LineSegment::from_line(w_vec, Black),
+    LineSegment::from_line(yellow_line, Orange),
+    LineSegment::from_line(white_line, Black),
   ])
 }
 
