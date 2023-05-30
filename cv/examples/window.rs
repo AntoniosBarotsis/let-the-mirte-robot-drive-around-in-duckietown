a15332ad3e9ef79124fdaf24bd1ace168c0557af
Kees Blok <C.Blok-1@student.tudelft.nl>
2023-05-30 15:41:53 +0200

    Fix pipeline

--- cv/examples/window.rs
+++ cv/examples/window.rs
@@ -1,3 +1,4 @@
+use cv::cv_error::CvError;
 use cv::detect_lines::detect_line_type;
 use cv::draw_lines::draw_lines;
 use cv::image::{downscale, read_image};
@@ -6,17 +7,19 @@ use std::time::Instant;
 
 /// Processes the input image from the assets folder and displays it in a window for an easier
 /// inspection. The window can also be closed by pressing any button.
-fn main() {
-  let img = read_image("./assets/input_1.jpg").expect("read image");
+fn main() -> Result<(), CvError> {
+  let img = read_image("./assets/input_1.jpg")?;
 
-  let mut resized = downscale(&img).expect("downscale");
+  let mut resized = downscale(&img)?;
 
   let now = Instant::now();
 
   let colours = vec![Yellow, White];
-  let lines = detect_line_type(&resized, colours).expect("process image");
+  let lines = detect_line_type(&resized, colours)?;
 
   println!("{:?}", now.elapsed());
 
   draw_lines(&mut resized, &lines);
+
+  Ok(())
 }
--- mirte-rs/examples/draw_lane.rs
+++ mirte-rs/examples/draw_lane.rs
@@ -11,8 +11,9 @@ fn main() {
   let mut img = read_image(&path).unwrap_or_else(|_| panic!("Unable to get image from {path}"));
 
   let colours = vec![cv::line::Colour::Yellow, cv::line::Colour::White];
+  #[allow(clippy::expect_used)]
   let lines = detect_line_type(&img, colours).expect("Unable to detect line with cv");
-
+  #[allow(clippy::expect_used)]
   let lane = detect_lane(&lines).expect("Unable to detect the lane");
 
   let drawn_lines = [lines, lane].concat();
