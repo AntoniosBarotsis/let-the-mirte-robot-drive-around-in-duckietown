3e1bd0fc2cfe2d815793b844e53bb2e8545bab77
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-06-03 20:59:03 +0200

    Fix typo

--- mirte-rs/examples/image_feed.rs
+++ mirte-rs/examples/image_feed.rs
@@ -8,7 +8,7 @@ use std::env;
 fn main() {
   let mut args = env::args();
   let path = args.nth(1).unwrap_or_else(|| {
-    println!("\nError: no input path given!\nExample usage: cargo r -r --example draw_lane ./assets/input_1.jpg\n");
+    println!("\nError: no input path given!\nExample usage: cargo r -r --example image_feed ./assets/input_1.jpg\n");
     std::process::exit(1);
   });
   let img = read_image(&path).unwrap_or_else(|_| panic!("Unable to get image from {path}"));
