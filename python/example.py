ba9360274291abff01d5ec3ed0753b7598d1cd48
Dennis Heijmans <D.Heijmans@student.tudelft.nl>
2023-06-07 16:10:08 +0200

    Fix typo

--- python/example.py
+++ python/example.py
@@ -1,12 +1,12 @@
 from cv import ImageProcessor
-proccessor = ImageProcessor()
+processor = ImageProcessor()
 
 while True:
   # Get the line segments
-  line_segments = proccessor.getLines()
+  line_segments = processor.getLines()
   # Print them
   for segment in line_segments:
     print(segment)
   print()
   # Sleep for 1/30th of a second
-  proccessor.sleep()
+  processor.sleep()
