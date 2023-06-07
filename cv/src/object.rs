e45df34fce14cfa099ed1c8336d3a180b07151a3
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-06-07 17:00:04 +0200

    Remove unsafe block

--- cv/src/object.rs
+++ cv/src/object.rs
@@ -1,10 +1,8 @@
-use std::mem;
-
 use opencv::{
   core::{KeyPoint, Scalar, Vector},
-  features2d::{draw_keypoints, Feature2D, SimpleBlobDetector, SimpleBlobDetector_Params},
+  features2d::{draw_keypoints, SimpleBlobDetector, SimpleBlobDetector_Params},
   highgui::{imshow, wait_key},
-  prelude::{Boxed, Feature2DTrait, Mat},
+  prelude::{Feature2DTrait, Mat},
 };
 
 use crate::{cv_error::CvError, image::convert_to_gray};
@@ -13,25 +11,16 @@ use crate::{cv_error::CvError, image::convert_to_gray};
 pub fn get_road_binary(img: &Mat) -> Result<(), CvError> {
   let img_gray = convert_to_gray(img)?;
 
-  let mut blob_detector: Feature2D;
   let params = SimpleBlobDetector_Params::default()?;
 
   let mut keypoints: Vector<KeyPoint>;
 
-  #[allow(unsafe_code)]
-  unsafe {
-    let mut blob_detector_ptr = SimpleBlobDetector::create(params)?;
-    let raw_ptr = blob_detector_ptr.as_raw_mut_Feature2D();
-    blob_detector = Feature2D::from_raw(raw_ptr);
+  let mut blob_detector_ptr = SimpleBlobDetector::create(params)?;
 
-    keypoints = Vector::<KeyPoint>::new();
-    println!("before");
-    blob_detector.detect(&img_gray, &mut keypoints, &Mat::default())?;
-    println!("after");
-
-    #[allow(clippy::mem_forget)]
-    mem::forget(blob_detector);
-  };
+  keypoints = Vector::<KeyPoint>::new();
+  println!("before");
+  blob_detector_ptr.detect(&img_gray, &mut keypoints, &Mat::default())?;
+  println!("after");
 
   let mut output_img = Mat::default();
   draw_keypoints(
