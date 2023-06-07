aaaa3920533d331654bedd0a5bc503104c05e0e0
Bryan <b.b.vandermark@student.tudelft.nl>
2023-06-07 16:16:12 +0200

    OpenCV image conversion

--- python/camera_feed.py
+++ python/camera_feed.py
@@ -1,8 +1,11 @@
 import rospy
 from sensor_msgs.msg import Image
+import cv2  # Import needed since otherwise CvBridge crashes
+from cv_bridge import CvBridge
 
 class Camera:
     current_image: Image = None
+    bridge = CvBridge()
 
     def __init__(self):
         rospy.init_node("camera_py", anonymous=True)
@@ -15,5 +18,5 @@ class Camera:
         return self.current_image
 
     def get_opencv_image(self):
-        print("todo")
+        return self.bridge.imgmsg_to_cv2(self.current_image, desired_encoding="passthrough")
 
