8c8470a20ca16c1ca019f083c8daaabb16eeb040
Elias Hoste <e.r.hoste@student.tudelft.nl>
2023-06-10 21:50:22 +0200

    Fix pipeline

--- ros/src/param.rs
+++ ros/src/param.rs
@@ -7,20 +7,19 @@ use std::collections::HashMap;
 pub fn get_thresholds() -> HashMap<Colour, Threshold> {
   [Colour::White, Colour::Yellow, Colour::Red]
     .iter()
-    .map(|colour| {
-      // It should always be possible to resolve the parameter name, so this will never panic on
-      // the `expect` call.
-      #[allow(clippy::expect_used)]
-      let param =
-        rosrust::param(&format!("~thresholds/{}", colour.parameter_name())).expect(&format!(
-          "Unable to resolve parameter name '~thresholds/{}'",
-          colour.parameter_name()
-        ));
-      let threshold = param
-        .get()
-        .unwrap_or_else(|_| Threshold::by_colour(*colour));
-      let _ = param.set(&threshold);
-      (*colour, threshold)
+    .map(|&colour| {
+      let threshold = get_threshold(colour).unwrap_or_else(|| Threshold::by_colour(colour));
+      (colour, threshold)
     })
     .collect()
+}
+
+fn get_threshold(colour: Colour) -> Option<Threshold> {
+  let parameter_name = format!("~thresholds/{}", colour.parameter_name());
+  let param = rosrust::param(&parameter_name)?;
+  let threshold = param.get().ok()?;
+  param.set(&threshold).unwrap_or_else(|_| {
+    rosrust::ros_warn!("Unable to set parameter '{parameter_name}'");
+  });
+  threshold
 }
