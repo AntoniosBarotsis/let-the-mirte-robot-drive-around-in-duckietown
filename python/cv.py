d8f367c1a8b603623765da7db4cfd8bdfd594770
Dennis Heijmans <D.Heijmans@student.tudelft.nl>
2023-06-12 16:07:53 +0200

    Make Camera attributes private

--- python/cv.py
+++ python/cv.py
@@ -15,18 +15,18 @@ class Camera:
 
     def __init__(self):
         # Initialise line segments
-        self.line_segments = []
-        self.stop_line = None
+        self.__line_segments = []
+        self.__stop_line = None
 
         # Callback for line segments
         def line_segment_cb(data: LineSegmentMsg):
-            self.line_segments = []
+            self.__line_segments = []
             for segment in data.segments:
-                self.line_segments.append(LineSegment.fromMessage(segment))
+                self.__line_segments.append(LineSegment.fromMessage(segment))
 
         # Callback for stop line
         def stop_line_cb(data: LineMsg):
-            self.stop_line = Line.fromMessage(data)
+            self.__stop_line = Line.fromMessage(data)
 
         # Initialise node and subscribers
         rospy.init_node("camera", anonymous=True)
@@ -52,7 +52,7 @@ class Camera:
         Returns:
             list: List of LineSegment objects
         """
-        return self.line_segments
+        return self.__line_segments
 
     def getStopLine(self):
         """Gets the stop line from the camera
@@ -60,4 +60,4 @@ class Camera:
         Returns:
             Line: Stop line
         """
-        return self.stop_line
+        return self.__stop_line
