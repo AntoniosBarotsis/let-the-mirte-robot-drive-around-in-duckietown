9d5fdb72431333a2a8d7eb9b606de41b49131eef
Dennis Heijmans <D.Heijmans@student.tudelft.nl>
2023-06-17 22:57:57 +0200

    Correct name

--- python/example.py
+++ python/example.py
@@ -8,7 +8,7 @@ camera = duckietown.createCamera()
 mirte.setMotorSpeed("left", 60)
 mirte.setMotorSpeed("right", 60)
 while True:
-    if camera.stopLine():
+    if camera.seesStopLine():
         mirte.setMotorSpeed("left", 0)
         mirte.setMotorSpeed("right", 0)
     time.sleep(0.03)
