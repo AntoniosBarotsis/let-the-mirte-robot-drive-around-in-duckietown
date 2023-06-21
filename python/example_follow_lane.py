478fa242152024f5e2b68bbed321ac1542152a2c
Bryan <b.b.vandermark@student.tudelft.nl>
2023-06-21 15:29:01 +0200

    Add example

---
+++ python/example_follow_lane.py
@@ -0,0 +1,11 @@
+# pylint: skip-file
+
+from mirte_robot import robot
+from mirte_duckietown import duckietown
+mirte = robot.createRobot()
+camera = duckietown.createCamera(mirte)
+
+
+while not (camera.seesStopLine()):
+    camera.startFollowing()
+camera.stopFollowing()
